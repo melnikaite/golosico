fe8875a826d7846312abd31078de7934e0184d7f
Eugene Melnikov <melnikaite.melnikaite@gmail.com>
2017-12-09 21:52:38 +0300

    fix asset create

--- ico.js
+++ ico.js
@@ -1,64 +1,65 @@
 golos.config.set('websocket', 'wss://ws.testnet.golos.io');
+golos.config.set('address_prefix', 'GLS');
 golos.config.set('chain_id', '5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679');
 
 const ico = {
+  //ico.createPost({password: '', author: 'melnikaite', maintag: 'maintag', permalink: 'permalink', title: 'title', body: 'body'}, console.log);
   createPost: (options, callback) => {
-    golos.broadcast.comment(options.postingKey, '', options.maintag, options.author, options.permalink, options.title, options.body, {}, callback);
+    const wif = golos.auth.toWif(options.author, options.password, 'posting');
+    golos.broadcast.comment(wif, '', options.maintag, options.author, options.permalink, options.title, options.body, {}, callback);
   },
 
-  //ico.createAsset({activeKey: '', author: 'melnikaite', assetName: 'TESTTEST0', precision: 2}, console.log);
+  //ico.createAsset({password: '', author: 'melnikaite', assetName: 'TESTTEST', precision: 2, supply: 1000000, golosAmount: 2, assetAmount: 100}, console.log);
   createAsset: (options, callback) => {
-    golos.broadcast.assetCreate(options.activeKey, options.author, options.assetName, options.precision, {
-      max_supply: 100000000,
-      market_fee_percent: 0,
-      max_market_fee: 0,
-      // charge_market_fee = 0x01, /**< an issuer-specified percentage of all market trades in this asset is paid to the issuer */
-      // white_list = 0x02, /**< accounts must be whitelisted in order to hold this asset */
-      // override_authority = 0x04, /**< issuer may transfer asset back to himself */
-      // transfer_restricted = 0x08, /**< require the issuer to be one party to every transfer */
-      // disable_force_settle = 0x10, /**< disable force settling */
-      // global_settle = 0x20, /**< allow the bitasset issuer to force a global settling -- this may be set in permissions, but not flags */
-      // disable_confidential = 0x40, /**< allow the asset to be used with confidential transactions */
-      // witness_fed_asset = 0x80, /**< allow the asset to be fed by witnesses */
-      // committee_fed_asset = 0x100 /**< allow the asset to be fed by the committee */
-      issuer_permissions: 79,
-      flags: 0,
-      core_exchange_rate: {
-        base: '1.000 GOLOS',
-        quote: '1.000 TESTTEST1',
-      },
-      whitelist_authorities: [],
-      blacklist_authorities: [],
-      whitelist_markets: [],
-      blacklist_markets: [],
-      description: '{"main":"","market":""}',
-      extensions: [],
-    }, {
-      short_backing_asset: '',
-      feed_lifetime_sec: 0,
-      force_settlement_delay_sec: 0,
-      minimum_feeds: 0,
-      force_settlement_offset_percent: 0,
-      maximum_force_settlement_volume: 0,
-      extensions: [],
-    }, false, [], (err, result) => {
-      callback(err, result);
-    });
+    const wif = golos.auth.toWif(options.author, options.password, 'active');
+    const operations = [
+      ['asset_create', {
+        issuer: options.author,
+        asset_name: options.assetName,
+        precision: options.precision,
+        common_options: {
+          max_supply: options.supply * Math.pow(10, options.precision),
+          market_fee_percent: 0,
+          max_market_fee: "0",
+          issuer_permissions: 79,
+          flags: 0,
+          core_exchange_rate: {
+            base: `${options.golosAmount}.000 GOLOS`,
+            quote: `${options.assetAmount}.000 ${options.assetName}`
+          },
+          whitelist_authorities: [],
+          blacklist_authorities: [],
+          whitelist_markets: [],
+          blacklist_markets: [],
+          description: `{"main":"${options.description || ''}",short_name:"${options.shortDescription || ''}","market":""}`,
+          extensions: []
+        }, 'is_prediction_market': false, 'extensions': []
+      }]
+    ];
+    golos.broadcast.send({extensions: [], operations}, [wif], callback);
   },
 
   getAsset: (assetName, callback) => {
     golos.api.getAssets([assetName], callback);
   },
 
+  //ico.issueAsset({password: '', issuer: 'melnikaite', amount: '100.000', assetName: 'AAAAAAAA'}, console.log);
   issueAsset: (options, callback) => {
-    golos.broadcast.assetIssue(options.activeKey, options.issuer, `${options.amount} ${options.assetName}`, options.issuer, '', [], callback);
+    const wif = golos.auth.toWif(options.issuer, options.password, 'active');
+    golos.broadcast.assetIssue(wif, options.issuer, `${options.amount} ${options.assetName}`, options.issuer, '', [], callback);
   },
 
+  //ico.sendToMarket({password: '', issuer: 'melnikaite', amountGolos: 2, amountAsset: '100.000', assetName: 'AAAAAAAA', expiration: new Date(new Date().getTime() + 24 * 60 * 60 * 1000)}, console.log);
   sendToMarket: (options, callback) => {
-
+    const wif = golos.auth.toWif(options.issuer, options.password, 'active');
+    golos.broadcast.limitOrderCreate(wif, options.issuer, Date.now(), `${options.amountGolos} GOLOS`, `${options.amountAsset} ${options.assetName}`, false, options.expiration, callback);
   },
 
+  //ico.sendToMarket({password: '', issuer: 'melnikaite', amountGolos: 2, amountAsset: '100.000', assetName: 'AAAAAAAA', expiration: new Date(new Date().getTime() + 24 * 60 * 60 * 1000)}, console.log);
   buyFromMarket: (options, callback) => {
-
+    const wif = golos.auth.toWif(options.issuer, options.password, 'active');
+    golos.broadcast.limitOrderCreate(wif, options.issuer, Date.now(), `${options.amountAsset} ${options.assetName}`, `${options.amountGolos} GOLOS`, false, options.expiration, callback);
   },
+
+  //todo: get_limit_orders_by_owner/ getorderbook
 };
